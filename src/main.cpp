044762f0b046fdea9eb9ec5cb301cba351868f2e
Daniel Brittain <djbrittain2017@gmail.com>
2025-05-11 23:58:41 -0700

    Reorganized code to focus more onh the shader parts of the code. Put init stuff into functions, and create a struct object that has a window* and opengl context.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 // --- New Includes ---
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp> // For glm::value_ptr
+#include "SDL3/SDL_video.h"
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_sdl3.h"
@@ -17,126 +18,46 @@
 #include <fstream>
 #include <sstream>
 
-std::string LoadShaderSource(const char* filepath) {
-    std::ifstream file(filepath);
-    if(!file) {
-        std::cerr << "Failed to open shader file: " << filepath << "\n";
-        std::exit(-1);
-    }
+/*
+* Load Shader File. Located in src/shaders.
+*/
+std::string LoadShaderSource(const char* filepath);
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
-}
+static GLuint CompileShader(GLenum type, const char* src);
+static GLuint LinkProgram(GLuint vs, GLuint fs);
 
-static GLuint CompileShader(GLenum type, const char* src) {
-    GLuint s = glCreateShader(type);
-    glShaderSource(s, 1, &src, nullptr);
-    glCompileShader(s);
-    GLint ok = 0;
-    glGetShaderiv(s, GL_COMPILE_STATUS, &ok);
-    if (!ok) {
-        GLint logLength = 0;
-        glGetShaderiv(s, GL_INFO_LOG_LENGTH, &logLength);
-        std::vector<char> buf(logLength);
-        glGetShaderInfoLog(s, logLength, nullptr, buf.data());
-        std::cerr << "Shader compile error: " << buf.data() << "\n";
-        glDeleteShader(s);
-        std::exit(-1);
-    }
-    return s;
-}
+void SetGLAttributes();
+void InitSDL();
 
-static GLuint LinkProgram(GLuint vs, GLuint fs) {
-    GLuint p = glCreateProgram();
-    glAttachShader(p, vs);
-    glAttachShader(p, fs);
-    glLinkProgram(p);
-    GLint ok = 0;
-    glGetProgramiv(p, GL_LINK_STATUS, &ok);
-    if (!ok) {
-        GLint logLength = 0;
-        glGetProgramiv(p, GL_INFO_LOG_LENGTH, &logLength);
-        std::vector<char> buf(logLength);
-        glGetProgramInfoLog(p, logLength, nullptr, buf.data());
-        std::cerr << "Program link error: " << buf.data() << "\n";
-        glDeleteProgram(p);
-        std::exit(-1);
-    }
-    glDetachShader(p, vs);
-    glDetachShader(p, fs);
-    return p;
-}
+struct GLContext {
+    SDL_Window* window;
+    SDL_GLContext context;
+};
+
+GLContext InitSDLGL(const char* title, int width, int height);
+void PrintGLInfo();
+ImGuiIO& InitIMGUI(GLContext gl);
+void CleanupImgui();
+void CleanupSDL(GLContext gl);
 
 int main(int argc, char** argv) {
 
-    std::cout << "Working directory: " << std::filesystem::current_path() << "\n";
+    InitSDL();
+    SetGLAttributes();
+    GLContext gl = InitSDLGL("Dematik", 1024, 768);
+    PrintGLInfo();
+    //Init IMGUI
+    ImGuiIO& io = InitIMGUI(gl);
 
-    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
-        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
-        return -1;
-    }
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
-                        SDL_GL_CONTEXT_PROFILE_CORE);
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,
-                        SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
-
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL3 + GLAD + ImGui + GLM Test",
-        1024, 768,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
-    );
-    if (!window) {
-        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
-        SDL_Quit();
-        return -1;
-    }
-
-    SDL_GLContext ctx = SDL_GL_CreateContext(window);
-    if (!ctx) {
-        std::cerr << "SDL_GL_CreateContext failed: " << SDL_GetError() << "\n";
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
-    SDL_GL_MakeCurrent(window, ctx);
-    SDL_GL_SetSwapInterval(1);
-
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD\n";
-        SDL_GL_DestroyContext(ctx);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
-
-    std::cout << "OpenGL Vendor: " << glGetString(GL_VENDOR) << "\n";
-    std::cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << "\n";
-    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << "\n";
-    std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION)
-              << "\n";
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    (void)io;
-
-    ImGui::StyleColorsDark();
-    ImGuiStyle& style = ImGui::GetStyle();
-
-    const char* glsl_version = "#version 330 core"; // Match shader version
-    ImGui_ImplSDL3_InitForOpenGL(window, ctx);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
+    //Load shaders from file
     std::string vertexSource = LoadShaderSource("src/shaders/vertex.glsl");
     std::string fragmentSource = LoadShaderSource("src/shaders/fragment.glsl");
 
+    //Compile Shaders
     GLuint vs = CompileShader(GL_VERTEX_SHADER, vertexSource.c_str());
     GLuint fs = CompileShader(GL_FRAGMENT_SHADER, fragmentSource.c_str());
+
+    //Link Shaders
     GLuint program = LinkProgram(vs, fs);
     glDeleteShader(vs); // Delete shaders after linking
     glDeleteShader(fs);
@@ -147,6 +68,8 @@ int main(int argc, char** argv) {
         -0.5f, -0.5f, // Left
         0.5f,  -0.5f  // Right
     };
+
+    //Vertex array object, vertex buffer object
     GLuint vao, vbo;
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
@@ -184,7 +107,7 @@ int main(int argc, char** argv) {
                 case SDL_EVENT_WINDOW_RESIZED:
                 case SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED:
                     int w_px, h_px;
-                    SDL_GetWindowSizeInPixels(window, &w_px, &h_px);
+                    SDL_GetWindowSizeInPixels(gl.window, &w_px, &h_px);
                     glViewport(0, 0, w_px, h_px);
                     break;
                 default:
@@ -227,21 +150,157 @@ int main(int argc, char** argv) {
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 
-        SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(gl.window);
     }
 
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplSDL3_Shutdown();
-    ImGui::DestroyContext();
+    //Cleanup IMGUI
+    CleanupImgui();
 
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
     glDeleteProgram(program);
 
-    SDL_GL_DestroyContext(ctx);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    //Cleanup SDL
+    CleanupSDL(gl);
 
     return 0;
 }
 
+static GLuint LinkProgram(GLuint vs, GLuint fs) 
+{
+    GLuint p = glCreateProgram();
+    glAttachShader(p, vs);
+    glAttachShader(p, fs);
+    glLinkProgram(p);
+    GLint ok = 0;
+    glGetProgramiv(p, GL_LINK_STATUS, &ok);
+    if (!ok) {
+        GLint logLength = 0;
+        glGetProgramiv(p, GL_INFO_LOG_LENGTH, &logLength);
+        std::vector<char> buf(logLength);
+        glGetProgramInfoLog(p, logLength, nullptr, buf.data());
+        std::cerr << "Program link error: " << buf.data() << "\n";
+        glDeleteProgram(p);
+        std::exit(-1);
+    }
+    glDetachShader(p, vs);
+    glDetachShader(p, fs);
+    return p;
+}
+
+std::string LoadShaderSource(const char* filepath) 
+{
+    std::ifstream file(filepath);
+    if(!file) {
+        std::cerr << "Failed to open shader file: " << filepath << "\n";
+        std::exit(-1);
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+static GLuint CompileShader(GLenum type, const char* src) 
+{
+    GLuint s = glCreateShader(type);
+    glShaderSource(s, 1, &src, nullptr);
+    glCompileShader(s);
+    GLint ok = 0;
+    glGetShaderiv(s, GL_COMPILE_STATUS, &ok);
+    if (!ok) {
+        GLint logLength = 0;
+        glGetShaderiv(s, GL_INFO_LOG_LENGTH, &logLength);
+        std::vector<char> buf(logLength);
+        glGetShaderInfoLog(s, logLength, nullptr, buf.data());
+        std::cerr << "Shader compile error: " << buf.data() << "\n";
+        glDeleteShader(s);
+        std::exit(-1);
+    }
+    return s;
+}
+void SetGLAttributes() {
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+}
+void InitSDL(){
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
+        return;
+    }
+}
+GLContext InitSDLGL(const char* title, int width, int height) {
+    GLContext gl;
+
+    gl.window = SDL_CreateWindow(
+        title, width, height,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+
+    if (!gl.window) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        std::exit(-1);
+    }
+
+    gl.context = SDL_GL_CreateContext(gl.window);
+    if (!gl.context) {
+        std::cerr << "SDL_GL_CreateContext failed: " << SDL_GetError() << "\n";
+        SDL_DestroyWindow(gl.window);
+        SDL_Quit();
+        std::exit(-1);
+    }
+
+    SDL_GL_MakeCurrent(gl.window, gl.context);
+    SDL_GL_SetSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        SDL_GL_DestroyContext(gl.context);
+        SDL_DestroyWindow(gl.window);
+        SDL_Quit();
+        std::exit(-1);
+    }
+
+    return gl;
+}
+void PrintGLInfo(){
+
+    std::cout << "OpenGL Vendor: " << glGetString(GL_VENDOR) << "\n";
+    std::cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << "\n";
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << "\n";
+    std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION)
+              << "\n";
+}
+ImGuiIO& InitIMGUI(GLContext gl){
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+
+    ImGui::StyleColorsDark();
+    ImGuiStyle& style = ImGui::GetStyle();
+
+    const char* glsl_version = "#version 330 core"; // Match shader version
+    ImGui_ImplSDL3_InitForOpenGL(gl.window, gl.context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    return io;
+}
+void CleanupImgui(){
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+    return;
+}
+
+void CleanupSDL(GLContext gl){
+
+    SDL_GL_DestroyContext(gl.context);
+    SDL_DestroyWindow(gl.window);
+    SDL_Quit();
+
+    return;
+}
